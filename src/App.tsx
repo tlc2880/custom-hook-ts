a4a9d30a842efd40a5193bbbed607d75ff85fe7d
tlc2880 <tommylcao@gmail.com>
2023-07-15 17:51:07 -0700

    add custom hook useCounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,73 @@
-import React from 'react';
-import './App.css';
+import React, { useState} from 'react';
+import useCounter from "./useCounter";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const {
+    count,
+    // setCount,
+    increment,
+    decrement,
+    reset,
+    addVal,
+    setAddVal
+  } = useCounter(1);
+
+  const [multVal, setMultVal] = useState(2);
+  const [numArr, setNumArr] = useState([3, 4]);
+  const [inStr, setInStr] = useState("");
+  const [strArr, setStrArr] = useState<string[]>(["One", "Two"]);
+  const MultInput = () => {
+    let numArrTemp = [...numArr];
+    numArrTemp.forEach((item, index, arr) => {
+      numArrTemp[index] = item * multVal;
+    });
+    setNumArr(numArrTemp);
+  };
+  const pushString = () => {
+    setStrArr([...strArr, inStr]);
+    //obj.objStrArr = [...obj.objStrArr, inStr];
+    setInStr("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Count is {count}</p>
+      <h4 style={{ color: "blue" }}>Number array: {JSON.stringify(numArr)}</h4>
+      <h4 style={{ color: "green" }}>
+        String aArray: {JSON.stringify(strArr)}
+      </h4>
+      <button onClick={increment}>Inc</button>
+      <button onClick={decrement}>Dec</button>
+      <button onClick={reset}>Reset</button>
+      <input
+        type="number"
+        className={"textbox"}
+        value={addVal}
+        onChange={(e) => setAddVal(parseInt(e.target.value, 10))}
+      />
+      <input
+        type="number"
+        className={"textbox"}
+        value={multVal}
+        onChange={(e) => setMultVal(parseInt(e.target.value, 10))}
+      />
+      <button onClick={MultInput}>Multiply by input</button>|
+      <button
+        onClick={() => {
+          setNumArr([...numArr, numArr.length + 1]);
+          console.log(numArr);
+        }}
+      >
+        Add to array
+      </button>
+      <input
+        type="string"
+        className={"textbox"}
+        value={inStr}
+        onChange={(e) => setInStr(e.target.value)}
+      />
+      <button onClick={pushString}>Push string</button>|
+    </>
   );
 }
 
